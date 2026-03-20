698868b833d3fda2d9ff76ae19c9c668c01914e7
Oliver-Tippett <ot11@kent.ac.uk>
2026-03-20 16:19:58 +0000

    Take book Check book in list, remove from text file and fixed donate formatting

--- AssignmentTwo.py
+++ AssignmentTwo.py
@@ -18,13 +18,15 @@ def main():
                   5. Exit """))
         if choice == 1:
             book_list()
+
         elif choice == 2:    
-            books = get_books() #call get_books function
-            print (f"Book: {books.name} | By: {books.author} | Genre: {books.genre}") 
+            take_books() #call get_books function 
+
         elif choice == 4:
             book = get_books()
             donateBooks = donate_books(book)
             print("Book donated successfully ")
+
 def book_list():
     try:
         with open("Book_name.txt", "r") as file:
@@ -32,6 +34,32 @@ def book_list():
             print (lines)
     except FileNotFoundError:
         print("Cant find file")
+
+def take_books():
+    book_name = input("Enter the name of the book you want to take: ").lower() #Convert input to all lowercase
+    try:
+        with open("Book_name.txt", "r") as file: 
+            books = file.readlines()
+        
+        found = False # Book not found yet 
+
+        with open("Book_name.txt", "w") as file: 
+            for book in books:
+                name, author, genre = book.strip().split(" - ")
+
+                if name.lower() == book_name and not found:
+                    print(f"You took: {name} | {author} | {genre}")
+                    found = True
+                    continue
+
+                file.write(book)
+
+        if not found:
+            print("This book was not found ")
+
+    except FileNotFoundError:
+        print("Book file not found")
+    
 
 def get_books():
     name = input("Enter Book name ") 
@@ -41,8 +69,7 @@ def get_books():
 
 def donate_books(book):
     with open("Book_name.txt", "a") as file:
-        file.write(f"{book.name},{book.author},{book.genre}")
-
+        file.write(f"{book.name} - {book.author} - {book.genre}\n")
 
 if __name__ == "__main__":
     main()
